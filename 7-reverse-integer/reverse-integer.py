b13b79c0aade1f1b90cf74b50473eccd089b1268
PRIYADARSHINI RAVIKUMAR <sankarisivaravi@gmail.com>
2025-03-18 22:01:50 +0530

    Time: 15 ms (76.46%) | Memory: 12.5 MB (7.57%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reverse(self, x):
+        MIN = -2147483648  
+        MAX = 2147483647   
+
+        res = 0
+        sign = -1 if x < 0 else 1  
+        x = abs(x)  
+        
+        while x:
+            digit = x % 10
+            x = x // 10
+
+           
+            if res > MAX // 10 or (res == MAX // 10 and digit > MAX % 10):
+                return 0
+
+            res = res * 10 + digit
+
+        return res * sign
